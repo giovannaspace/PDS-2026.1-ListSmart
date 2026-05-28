326b719094d33af9ba00023ae8c73af43a4f3211
Giovanna <you@example.com>
2026-05-27 21:10:41 -0300

    chamadas facade

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ gerente = GerenciamentoCategoria()
 gerente.categoria()
 gerente.dicionario()
 
+from facade import *
 
 # ================= LOGIN =================
 @app.route("/", methods=["GET", "POST"])
@@ -24,7 +25,8 @@ def login():
         email = request.form["email"]
         senha = request.form["senha"]
 
-        usuario = Usuario.login(email, senha)
+        # CHAMADA FACADE
+        usuario = Facade.login_facade(email,senha)
 
         if usuario:
             session["idUsuario"] = usuario.idUsuario
@@ -34,6 +36,9 @@ def login():
 
     return render_template("login.html")
 
+
+
+
 # ================= CADASTRO =================
 @app.route("/cadastro", methods=["GET","POST"])
 def cadastroView():
@@ -42,7 +47,9 @@ def cadastroView():
         email = request.form["email"]
         senha = request.form["senha"]
 
-        resultado = cadastro(nome,email,senha)
+        # CHAMADA FACADE
+        resultado = Facade.cadastro_facade(nome,email,senha)
+
         if resultado:
             return redirect(url_for("login"))
         else:
@@ -57,10 +64,8 @@ def listas():
     if "idUsuario" not in session:
         return redirect("/")
     
-    listas = buscarLista(session["idUsuario"])
-
-    for lista in listas:
-        lista["quantidade"] = contarItens(lista["id"])
+    # CHAMADA FACADE
+    listas = Facade.listas_facade(session["idUsuario"])
 
     return render_template("listas.html",listas=listas)
 
@@ -72,31 +77,32 @@ def criarLista():
         idUsuario = session["idUsuario"]
         obs = request.form["observacoes"]
 
-        novaLista = Lista(nomeLista=nome, tetoOrcamentario=teto, observacoes=obs,idDono=idUsuario)
-        ListaNovaBD(novaLista)
+        # CHAMADA FACADE
+        Facade.criarListas_facade(nome,teto,obs,idUsuario)
+        #novaLista = Lista(nomeLista=nome, tetoOrcamentario=teto, observacoes=obs,idDono=idUsuario)
+        #ListaNovaBD(novaLista)
 
         return redirect(url_for("listas"))
 
-    categorias = gerente.sugestaoNomeLista()
-
+    #categorias = gerente.sugestaoNomeLista()
+    categorias = Facade.sugestao_categoriaLista_facade()
     return render_template("telaCriarLista.html",categorias=categorias)
 
 @app.route("/itens/<idLista>")
 def itens(idLista):
 
-    listaRec = Lista.recontruirLista(idLista)
-    marcados = listaRec.calcularTotalMarcados() or 0
-    pendentes = listaRec.calcularTotalPendentes() or 0
-    totalGeral = listaRec.calcularTotalGeral() or 0
+    listaRec,marcados,pendentes,totalGeral,tetoInfo,itens = Facade.itens_facade(idLista)
+   # listaRec = Lista.recontruirLista(idLista)
+   # marcados = listaRec.calcularTotalMarcados() or 0
+   # pendentes = listaRec.calcularTotalPendentes() or 0
+   # totalGeral = listaRec.calcularTotalGeral() or 0
 
-    tetoInfo = listaRec.calcularTetoOrcamentario()
+   # tetoInfo = listaRec.calcularTetoOrcamentario()
 
-    itens = listaRec.itensDaLista()
+   # itens = listaRec.itensDaLista()
 
     return render_template("itens.html",lista=listaRec, itens=itens,idLista=idLista,marcados=marcados,pendentes=pendentes,totalGeral=totalGeral,tetoInfo=tetoInfo)
 
-
-'''
 @app.route("/criarItem/<idLista>", methods=["GET","POST"])
 def criarItem(idLista):
 
@@ -111,77 +117,57 @@ def criarItem(idLista):
         temPromo = request.form["temPromocao"]
         tipo = request.form.get("tipoPromocao")
 
-        if temPromo == "sim":
-            if tipo == "porcentagem":
-                porcentagem = int(request.form.get("porcentagem") or 0)
-                Lista.adicionarItem(idLista,nome, qtd, unidade, preco,obs,categoria,"s",1,0,0,porcentagem)
-
-            elif tipo == "leve":
-                x = int(request.form.get("leveX") or 0)
-                y = int(request.form.get("pagueY") or 0)
-                Lista.adicionarItem(idLista,nome, qtd, unidade, preco,obs,categoria,"s",2,x,y,0)
-        else:
-            Lista.adicionarItem(idLista,nome, qtd, unidade, preco,obs,categoria,"n",0,0,0,0)
-
-        return redirect(url_for("itens",idLista=idLista))
-                
-    categorias = gerente.sugestaoNomeLista()
-    catItens = gerente.sugestaoNomeItem()
-    return render_template("telaCriarItem.html",categorias=categorias,catItens=catItens,idLista=idLista)
-'''
-@app.route("/criarItem/<idLista>", methods=["GET","POST"])
-def criarItem(idLista):
-
-    if request.method == "POST":
-        nome = request.form["nomeItem"]
-        qtd = int(request.form["quantidade"])
-        unidade = request.form["unidade"]
-        preco = float(request.form["preco"])
-        categoria = request.form["categoria"]
-        obs = request.form["observacao"]
-
-        temPromo = request.form["temPromocao"]
-        tipo = request.form.get("tipoPromocao")
-
-        builder = itemBuilder()
+    #    builder = itemBuilder()
 
         if temPromo == "sim":
+
             if tipo == "porcentagem":  #tipo 1
+                
+                # CHAMADA FACADE
                 porcentagem = int(request.form.get("porcentagem") or 0)
-                desconto_calculo = tipoDesconto(preco,1,0,0,porcentagem)
+                Facade.criarItem_facade(idLista,1,nome,qtd,unidade,preco,categoria,obs,tipo,porcentagem,0,0)
+
+
+                #desconto_calculo = tipoDesconto(preco,1,0,0,porcentagem)
                  # debug print(desconto_calculo)
-                director = builderDirector(builder, nome, qtd, unidade, preco, obs, categoria, idLista, desconto_calculo, 0)
+                #director = builderDirector(builder, nome, qtd, unidade, preco, obs, categoria, idLista, desconto_calculo, 0)
 
                 #director = builderDirector(builder,nome,qtd,unidade,preco,desconto_calculo,obs,0,idLista,categoria)
-                item = director.get_Promo()
-                ItemNovaBD(item) # antes estava no metodo adicionarItem da classe Lista
+                    #item = director.get_Promo()
+                    #ItemNovaBD(item) # antes estava no metodo adicionarItem da classe Lista
                 #Lista.adicionarItem(idLista,nome, qtd, unidade, preco,obs,categoria,"s",1,0,0,porcentagem)
 
             elif tipo == "leve": #tipo 2
                 x = int(request.form.get("leveX") or 0)
                 y = int(request.form.get("pagueY") or 0)
-                desconto_calculo = tipoDesconto(preco,2,x,y,0)
+
+                # CHAMADA FACADE
+                Facade.criarItem_facade(idLista,1,nome,qtd,unidade,preco,categoria,obs,tipo,0,x,y)
+
+
+
+              #  desconto_calculo = tipoDesconto(preco,2,x,y,0)
 
                 # debug print(desconto_calculo)
-                director = builderDirector(builder, nome, qtd, unidade, preco, obs, categoria, idLista, desconto_calculo, 0)
+             #   director = builderDirector(builder, nome, qtd, unidade, preco, obs, categoria, idLista, desconto_calculo, 0)
                 #director = builderDirector(builder,nome,qtd,unidade,preco,desconto_calculo,obs,0,idLista,categoria)
-                item = director.get_Promo()
-                ItemNovaBD(item) # antes estava no metodo adicionarItem da classe Lista
+            #    item = director.get_Promo()
+               # ItemNovaBD(item) # antes estava no metodo adicionarItem da classe Lista
 
                 #Lista.adicionarItem(idLista,nome, qtd, unidade, preco,obs,categoria,"s",2,x,y,0)
         else:
-            director = builderDirector(builder, nome, qtd, unidade, preco, obs, categoria, idLista, 0, 0)
+            # CHAMADA FACADE
+            Facade.criarItem_facade(idLista,0,nome,qtd,unidade,preco,categoria,obs,tipo,0,0,0)
 
-            #director = builderDirector(builder,nome,qtd,unidade,preco,0,obs,0,idLista,categoria)
-            item = director.get_Comum()
-            ItemNovaBD(item) # antes estava no metodo adicionarItem da classe Lista
-
-            #Lista.adicionarItem(idLista,nome, qtd, unidade, preco,obs,categoria,"n",0,0,0,0)
 
         return redirect(url_for("itens",idLista=idLista))
                 
-    categorias = gerente.sugestaoNomeLista()
-    catItens = gerente.sugestaoNomeItem()
+   # categorias = gerente.sugestaoNomeLista()
+   # catItens = gerente.sugestaoNomeItem()
+
+    # CHAMADA FACADE
+    categorias = Facade.sugestao_categoriaLista_facade()
+    catItens = Facade.sugestao_categoriaItem_facade()
     return render_template("telaCriarItem.html",categorias=categorias,catItens=catItens,idLista=idLista)
 
 
@@ -190,8 +176,10 @@ def marcarItem(idLista):
 
     idItem = request.form["idItem"]
 
-    listaRec = Lista.recontruirLista(idLista)
-    listaRec.alterarStatus(idItem)
+    # CHAMADA FACADE
+    Facade.marcar_item_facade(idLista,idItem)
+   # listaRec = Lista.recontruirLista(idLista)
+   # listaRec.alterarStatus(idItem)
 
     return redirect(url_for("itens",idLista=idLista))
 
@@ -202,7 +190,6 @@ def item(idItem):
 
     if request.method == "POST":
 
-        
         nome = request.form.get("nome")
         quantidade = request.form.get("quantidade")
         unidade = request.form.get("unidade")
@@ -210,37 +197,9 @@ def item(idItem):
         categoria = request.form.get("categoria")
         nota = request.form.get("nota")
 
-        
-        if nome:
-            itemRec.atualizarItem("nome", nome)
-
-        if unidade:
-            itemRec.atualizarItem("unidade", unidade)
-
-        if preco:
-            itemRec.atualizarItem("preco", float(preco))
-
-        if nota:
-            itemRec.atualizarItem("nota", nota)
-
-        import sqlite3
-        conexao = sqlite3.connect("banco.db")
-        cursor = conexao.cursor()
-
-        if quantidade:
-            cursor.execute("""
-                UPDATE itens SET quantidade = ?
-                WHERE idItem = ? AND idDonoItem = ?
-            """, (int(quantidade), itemRec.idItem, itemRec.idDonoItem))
-
-        if categoria:
-            cursor.execute("""
-                UPDATE itens SET idCategoria = ?
-                WHERE idItem = ? AND idDonoItem = ?
-            """, (categoria, itemRec.idItem, itemRec.idDonoItem))
-
-        conexao.commit()
-        conexao.close()
+        # CHAMADA FACADE
+        Facade.item_caracteristicas_facade(idItem,nome,quantidade,unidade,preco,categoria,nota)
+        #atualizar_quant_e_categoria(itemRec,quantidade,categoria)
 
         return redirect(url_for("listas"))
 
@@ -249,22 +208,24 @@ def item(idItem):
 @app.route("/item/deletar/<idItem>", methods=["POST"])
 def deletarItem(idItem):
 
-    itemRec = Item.reconstruirItem(idItem)
+   # itemRec = Item.reconstruirItem(idItem)
 
-    if itemRec:
-        itemRec.removerItem()
-
+    #if itemRec:
+     #   itemRec.removerItem()
+    # CHAMADA FACADE
+    Facade.deletar_item_facade(idItem)
     return redirect(url_for("listas"))
 
 @app.route("/compartilhar/email/<idLista>", methods=["POST"])
 def compartilhar_email(idLista):
  
-    listaRec = Lista.recontruirLista(idLista)
+    #listaRec = Lista.recontruirLista(idLista)
 
     email = request.form.get("email")
-
-    comp = compartilhamento(listaRec)
-    comp.enviarEmail(email)
+    # CHAMADA FACADE
+    Facade.email_facade(idLista,email)
+    #comp = compartilhamento(listaRec)
+    #comp.enviarEmail(email)
 
     return redirect(url_for("listas", idLista=idLista))
 
@@ -272,12 +233,12 @@ def compartilhar_email(idLista):
 @app.route("/compartilhar/zap/<idLista>", methods=["POST"])
 def compartilhar_zap(idLista):
 
-    listaRec = Lista.recontruirLista(idLista)
+    #listaRec = Lista.recontruirLista(idLista)
 
     numero = request.form.get("numero")
-
-    comp = compartilhamento(listaRec)
-    comp.enviarZap(numero)
+    Facade.zap_facade(idLista,numero)
+    #comp = compartilhamento(listaRec)
+    #comp.enviarZap(numero)
 
     return redirect(url_for("listas", idLista=idLista))
 
